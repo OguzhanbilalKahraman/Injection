a43a419346bae7bac4cfb1dad00a991f3efbe662
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-22 14:48:55 +0300

    Add files via upload

---
+++ InjectionAndIOC/InjectionAndIOC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using InjectionAndIOC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InjectionAndIOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            MsSqlOperation mssql = new MsSqlOperation();
+            mssql.Connection();
+
+
+            ISqlOperation sql = new MsSqlOperation();
+            sql.Connection();
+
+            ISqlOperation mysql = new MsSqlOperation();
+            mysql.Connection();
+
+
+            return View();
+        }
+    }
+}
---
+++ InjectionAndIOC/InjectionAndIOC/Models/MsSqlOperation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InjectionAndIOC.Models
+{
+    public class MsSqlOperation : Test, ISqlOperation, ITestInterface
+    {
+        public void Connection()
+        {
+            Console.WriteLine("MsSql bağlantısı sağlandı.");
+
+        }
+
+        public bool Insert(Product product)
+        {
+            return true;
+        }
+    }
+
+    public class MySqlOperation : ISqlOperation
+    {
+        public void Connection()
+        {
+            Console.WriteLine("MySql bağlantısı sağlandı."); 
+        }
+
+        public bool Insert(Product product)
+        {
+            return true;
+        }
+    }
+
+
+
+    public interface ISqlOperation
+    {
+        void Connection();
+
+        bool Insert(Product product);
+
+    }
+
+    public class Test
+    {
+
+
+    }
+
+    public interface ITestInterface
+    {
+
+    }
+
+
+}
